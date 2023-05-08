03ce5ca462750372a3207509bcf2f248ccfb0a70
Jeremiah K <jeremiahk@riseup.net>
2023-05-08 17:45:29 -0500

    Formatting changes

--- main.py
+++ main.py
@@ -113,7 +113,7 @@ class BibleBot:
             await self.send_reaction(room_id, event.event_id, "✅")
 
             # Add the string of emojis (🕊️✝️ 📔) at the end of the reference
-            message = f"{text} - {reference} 🕊️✝️ 📔⚔️"
+            message = f"{text} - {reference} 🕊️✝️"
             await self.client.room_send(
                 room_id,
                 "m.room.message",
